f914772d8ad8da62ef828c50e665e9e3ab336418
Michael Haberler <git@mah.priv.at>
2024-06-01 06:11:11 +0200

    state of affairs

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,8 +1,32 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const zlib = require('zlib')
 
 process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
+  pluginOptions: {
+    // https://github.com/webpack-contrib/compression-webpack-plugin/tree/v6.1.1
+    compression: {
+      // brotli: {
+      //   filename: '[file].br[query]',
+      //   algorithm: 'brotliCompress',
+      //   include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+      //   compressionOptions: {
+      //     params: {
+      //       [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+      //     },
+      //   },
+      //   minRatio: 0.8,
+      // },
+      gzip: {
+        filename: '[file].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json|map)(\?.*)?$/i,
+        minRatio: 0.8,
+        deleteOriginalAssets: true,
+      },
+    },
+  },
   publicPath: process.env.BASE_URL,
   outputDir: process.env.VUE_APP_OUTPUT_DIR,
   configureWebpack: {
